063df015711074d73bbfa822b684ffa341563f0f
Yashmodi290 <ymodi039@gmail.com>
2026-05-12 07:25:25 +0000

    updated project

---
+++ DataTypesExample.java
@@ -0,0 +1,36 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        // boolean
+        boolean isJavaEasy = true;
+        System.out.println("Boolean: " + isJavaEasy);
+
+        // byte
+        byte age = 20;
+        System.out.println("Byte: " + age);
+
+        // char
+        char grade = 'A';
+        System.out.println("Char: " + grade);
+
+        // short
+        short marks = 32000;
+        System.out.println("Short: " + marks);
+
+        // int
+        int salary = 50000;
+        System.out.println("Int: " + salary);
+
+        // long
+        long population = 9876543210L;
+        System.out.println("Long: " + population);
+
+        // float
+        float price = 99.5f;
+        System.out.println("Float: " + price);
+
+        // double
+        double percentage = 89.456;
+        System.out.println("Double: " + percentage);
+    }
+}
