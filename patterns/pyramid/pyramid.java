6d01bde1a7bb709f630941b04f012174fc3cc785
Yashmodi290 <ymodi039@gmail.com>
2026-05-13 19:32:50 +0000

    patterns

---
+++ patterns/pyramid/pyramid.java
@@ -0,0 +1,22 @@
+public class pyramid {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
