a8e8e092476899662308a85475d27fc8bcdb4886
Yashmodi290 <ymodi039@gmail.com>
2026-05-13 19:37:57 +0000

    petterns

---
+++ patterns/inverted_right/inverted_right.java
@@ -0,0 +1,15 @@
+public class inverted_right{
+    public static void main(String[] args){
+        int n = 5;
+         for(int i = 5;i>1;i--){
+            for(int j =1; j<=i;j++){
+                
+                System.out.print("* ");
+               
+            }
+            System.out.println();   
+         }
+               
+    }
+
+}
